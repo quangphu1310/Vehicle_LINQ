a577ffedb758cb2fa72a2a5168be57c3e36822e2
quangphu <trancongquangphu10@gmail.com>
2024-03-11 22:36:47 +0700

    assignment 3

---
+++ BaiTap3_LTCS_Employee/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap3_LTCS_Employee
+{
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public int DepartmentID { get; set; }
+
+        public static List<Employee> GetEmployees()
+        {
+            return new List<Employee>() {
+                new Employee {ID = 1, Name = "A", Age = 20,Salary = 1000000, Birthday =  new DateTime(2004, 12, 13), DepartmentID = 1},
+                new Employee {ID = 2, Name = "D", Age = 21, Salary = 2000000, Birthday = new DateTime(2003, 2, 13), DepartmentID = 2},
+                new Employee {ID = 3, Name = "C", Age = 20, Salary = 2500000, Birthday = new DateTime(2004, 4, 17), DepartmentID = 3},
+                new Employee {ID = 4, Name = "B", Age = 22, Salary = 5000000, Birthday = new DateTime(2002, 8, 5), DepartmentID = 4},
+                new Employee {ID = 5, Name = "E", Age = 23, Salary = 3500000, Birthday = new DateTime(2001, 3, 3), DepartmentID = 5},
+            };
+        }
+    }
+}
---
+++ BaiTap3_LTCS_Employee/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap3_LTCS_Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<Employee> employees = Employee.GetEmployees();
+
+            //cau1
+            Console.WriteLine("Maximum Salary: " + employees.Max(e => e.Salary));
+            Console.WriteLine("Min Salary: " + employees.Min(e => e.Salary));
+            Console.WriteLine("Average Salary: " + employees.Average(e => e.Salary));
+
+            //cau2
+            // group join
+            var employeesByDepartment = Department.GetDepartments()
+                .GroupJoin(Employee.GetEmployees(),
+                    d => d.ID,
+                    e => e.DepartmentID,
+                    (d, eGroup) => new
+                    {
+                        Department = d,
+                        Employee = eGroup
+                    });
+
+            foreach (var department in employeesByDepartment)
+            {
+                Console.WriteLine(department.Department.Name);
+                foreach (var employee in department.Employee)
+                {
+                    Console.WriteLine(" " + employee.Name);
+                }
+                Console.WriteLine();
+            }
+
+            // left outer join
+            var result = Employee.GetEmployees()
+                .GroupJoin(Department.GetDepartments(),
+                    e => e.DepartmentID,
+                    d => d.ID,
+                    (e, dGroup) => new
+                    {
+                        employeeName = e.Name,
+                        DepartmentName = dGroup.FirstOrDefault()?.Name ?? "No Department"
+                    });
+
+            foreach (var employee in result)
+            {
+                Console.WriteLine("{0}-{1}", employee.employeeName, employee.DepartmentName);
+            }
+            Console.WriteLine();
+
+            // right outer join 
+            var result2 = Department.GetDepartments()
+                .GroupJoin(Employee.GetEmployees(),
+                    d => d.ID,
+                    e => e.DepartmentID,
+                    (d, eGroup) => new
+                    {
+                        DepartmentName = d.Name,
+                        EmployeeName = eGroup.FirstOrDefault()?.Name ?? "No Employee"
+                    });
+
+            foreach (var item in result2)
+            {
+                Console.WriteLine("{0}-{1}", item.EmployeeName, item.DepartmentName);
+            }
+            Console.WriteLine();
+
+            //cau3
+            var maxAge = employees.Max(e => (DateTime.Now - e.Birthday).Days / 365);
+            Console.WriteLine($"Max age: {maxAge} years");
+
+            var minAge = employees.Min(e => (DateTime.Now - e.Birthday).Days / 356);
+            Console.WriteLine($"Min age: {minAge} years");
+        }
+    }
+}
